ebd721ac27fa3accaf22ff27537b585ac821297f
zaineblamine <lamine_zeinab@yahoo.com>
2016-05-25 19:57:16 +0100

    localstorage done but with one notification

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -2,6 +2,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+        display()
     },
     bindEvents: function() {
         document.addEventListener('deviceready', function () {
@@ -10,9 +11,11 @@ var app = {
     var text=JSON.stringify(jsonData);
     var obj = JSON.parse(text);
     if (text!=""){
-      var myMessage=obj.message+ "<br>" +obj.additionalData.title;
-      localStorage.setItem("message", myMessage);
-    document.getElementById("notif").innerHTML = localStorage.getItem("message");
+      //var myMessage=obj.additionalData.title+" : "+obj.message;
+      var myMessage=obj.message;
+      localStorage.setItem("notif", myMessage);
+      localStorage.setItem("date", date_heure());
+      display();
 
 }
   };
@@ -56,3 +59,45 @@ var app = {
     }
 };
 app.initialize();
+
+function display(){
+  var notif=document.getElementById("notif");
+  var date=document.getElementById("date");
+  notif.innerHTML="";
+for(var x=0; x<localStorage.length;x++){
+
+  var b=localStorage.getItem('notif');
+  var c=localStorage.getItem('date');
+  notif.innerHTML=b;
+  date.innerHTML=c;
+}
+}
+function date_heure()
+{
+        date = new Date;
+        annee = date.getFullYear();
+        moi = date.getMonth();
+        mois = new Array('Janvier', 'F&eacute;vrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Ao&ucirc;t', 'Septembre', 'Octobre', 'Novembre', 'D&eacute;cembre');
+        j = date.getDate();
+        jour = date.getDay();
+        jours = new Array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+        h = date.getHours();
+        if(h<10)
+        {
+                h = "0"+h;
+        }
+        m = date.getMinutes();
+        if(m<10)
+        {
+                m = "0"+m;
+        }
+        s = date.getSeconds();
+        if(s<10)
+        {
+                s = "0"+s;
+        }
+        resultat = jours[jour]+' '+j+' '+mois[moi]+' '+annee+' à '+h+':'+m+':'+s;
+        /*document.getElementById(id).innerHTML = resultat;
+        setTimeout('date_heure("'+id+'");','1000');*/
+        return resultat;
+}
